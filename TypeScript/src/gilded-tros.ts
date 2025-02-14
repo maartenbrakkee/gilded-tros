f544d58afa3d1f029e8db1b5c218d7caa8c5e0c7
maartenbrakkee <1454803+maartenbrakkee@users.noreply.github.com>
2025-02-14 12:25:35 +0100

    refactor: updateQuality updated to match requirements

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -4,56 +4,50 @@ export class GildedTros {
   constructor(public items: Array<Item>) {}
 
   public updateQuality(): void {
-    for (const element of this.items) {
-      if (
-        element.name != "Good Wine" &&
-        element.name != "Backstage passes for Re:Factor" &&
-        element.name != "Backstage passes for HAXX"
-      ) {
-        if (element.quality > 0) {
-          if (element.name != "B-DAWG Keychain") {
-            element.quality = element.quality - 1;
-          }
-        }
-      } else if (element.quality < 50) {
-        element.quality = element.quality + 1;
+    // loop over all items
+    for (const item of this.items) {
+      // B-DAWG Keychain never has to be sold or decreases in quality: do nothing
+      if (item.name === "B-DAWG Keychain") continue;
 
-        if (element.name == "Backstage passes for Re:Factor") {
-          if (element.sellIn < 11) {
-            if (element.quality < 50) {
-              element.quality = element.quality + 1;
-            }
-          }
-
-          if (element.sellIn < 6) {
-            if (element.quality < 50) {
-              element.quality = element.quality + 1;
-            }
-          }
+      // quality of wine increases the older it gets
+      if (item.name === "Good Wine") {
+        item.quality++;
+        // specific scenarios for Backstage passes
+      } else if (item.name.includes("Backstage passes")) {
+        if (item.sellIn < 0) {
+          // Quality drops to 0 after the conference
+          item.quality = 0;
+        } else if (item.sellIn <= 5) {
+          // Quality increases by 3 when there are 5 days or less
+          item.quality += 3;
+        } else if (item.sellIn <= 10) {
+          // Quality increases by 2 when there are 10 days or less
+          item.quality += 2;
         }
+        // all other items decrease in quality
+      } else {
+        let qualityDelta = 1;
+
+        // Smelly items degrade in quality twice as fast as normal items
+        if (
+          ["Duplicate Code", "Long Methods", "Ugly Variable Names"].includes(
+            item.name,
+          )
+        )
+          qualityDelta = 2;
+
+        // Once the sell by date has passed, quality degrades twice as fast
+        if (item.sellIn < 0) item.quality -= 2 * qualityDelta;
       }
 
-      if (element.name != "B-DAWG Keychain") {
-        element.sellIn = element.sellIn - 1;
+      // The Quality of an item is never more than 50
+      if (item.quality > 50) {
+        item.quality = 50;
       }
 
-      if (element.sellIn < 0) {
-        if (element.name != "Good Wine") {
-          if (
-            element.name != "Backstage passes for Re:Factor" ||
-            element.name != "Backstage passes for HAXX"
-          ) {
-            if (element.quality > 0) {
-              if (element.name != "B-DAWG Keychain") {
-                element.quality = element.quality - 1;
-              }
-            }
-          } else {
-            element.quality = element.quality - element.quality;
-          }
-        } else if (element.quality < 50) {
-          element.quality = element.quality + 1;
-        }
+      //The Quality of an item is never negative
+      if (item.quality < 0) {
+        item.quality = 0;
       }
     }
   }
