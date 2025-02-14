efe36d4705d1b0b518cb464e7633674d1459288c
maartenbrakkee <1454803+maartenbrakkee@users.noreply.github.com>
2025-02-14 10:55:00 +0100

    test: fix initial test

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -1,9 +1,13 @@
+import { describe, expect, it } from "vitest";
+
 import { GildedTros } from "../src/gilded-tros";
 import { Item } from "../src/item";
 
 describe("GildedTrosTest", () => {
-  const items: Item[] = [new Item("foo", 0, 0)];
-  const app: GildedTros = new GildedTros(items);
-  app.updateQuality();
-  expect(app.items[0].name).toEqual("fixme");
+  it("first item outputted name should match the set item", async () => {
+    const items: Item[] = [new Item("foo", 0, 0)];
+    const app: GildedTros = new GildedTros(items);
+    app.updateQuality();
+    expect(app.items[0].name).toEqual("foo");
+  });
 });
