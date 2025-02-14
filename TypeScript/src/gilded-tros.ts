832dcc1002bb6de3702956984f2ef6bef3196483
maartenbrakkee <1454803+maartenbrakkee@users.noreply.github.com>
2025-02-14 11:22:23 +0100

    refactor: implement SonarCube recommendation
    use simple for-of loop and refactor else if statements

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -4,60 +4,55 @@ export class GildedTros {
   constructor(public items: Array<Item>) {}
 
   public updateQuality(): void {
-    for (let i = 0; i < this.items.length; i++) {
+    for (const element of this.items) {
       if (
-        this.items[i].name != "Good Wine" &&
-        this.items[i].name != "Backstage passes for Re:Factor" &&
-        this.items[i].name != "Backstage passes for HAXX"
+        element.name != "Good Wine" &&
+        element.name != "Backstage passes for Re:Factor" &&
+        element.name != "Backstage passes for HAXX"
       ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "B-DAWG Keychain") {
-            this.items[i].quality = this.items[i].quality - 1;
+        if (element.quality > 0) {
+          if (element.name != "B-DAWG Keychain") {
+            element.quality = element.quality - 1;
           }
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+      } else if (element.quality < 50) {
+        element.quality = element.quality + 1;
 
-          if (this.items[i].name == "Backstage passes for Re:Factor") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+        if (element.name == "Backstage passes for Re:Factor") {
+          if (element.sellIn < 11) {
+            if (element.quality < 50) {
+              element.quality = element.quality + 1;
             }
+          }
 
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+          if (element.sellIn < 6) {
+            if (element.quality < 50) {
+              element.quality = element.quality + 1;
             }
           }
         }
       }
 
-      if (this.items[i].name != "B-DAWG Keychain") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (element.name != "B-DAWG Keychain") {
+        element.sellIn = element.sellIn - 1;
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Good Wine") {
+      if (element.sellIn < 0) {
+        if (element.name != "Good Wine") {
           if (
-            this.items[i].name != "Backstage passes for Re:Factor" ||
-            this.items[i].name != "Backstage passes for HAXX"
+            element.name != "Backstage passes for Re:Factor" ||
+            element.name != "Backstage passes for HAXX"
           ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "B-DAWG Keychain") {
-                this.items[i].quality = this.items[i].quality - 1;
+            if (element.quality > 0) {
+              if (element.name != "B-DAWG Keychain") {
+                element.quality = element.quality - 1;
               }
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            element.quality = element.quality - element.quality;
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+        } else if (element.quality < 50) {
+          element.quality = element.quality + 1;
         }
       }
     }
