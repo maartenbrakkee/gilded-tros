57c0ff4a9996ce488eff9e657338cf037a3baa19
maartenbrakkee <1454803+maartenbrakkee@users.noreply.github.com>
2025-02-14 10:10:11 +0100

    feat: use .gitignore for ESLint

--- TypeScript/eslint.config.mjs
+++ TypeScript/eslint.config.mjs
@@ -1,9 +1,17 @@
-import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { includeIgnoreFile } from "@eslint/compat";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, "../.gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(gitignorePath),
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
